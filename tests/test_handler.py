ff3d597d132271f96f50a46490ef7039a9d262bf
Andrew Jennings <ajennings@azavea.com>
2012-06-15 17:28:10 -0400

    Update tests after merge
    Some of the updates from @superduper pulled in a slightly different format for
    the messages - using "levelname" instead of "level".  I updated the tests to
    conform to this new format.

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -20,53 +20,47 @@ class TestRootLoggerHandler(unittest.TestCase):
         self.db = self.conn[self.db_name]
         self.collection = self.db['log']
 
+        self.log = logging.getLogger('')
+        self.log.setLevel(logging.DEBUG)
+
+        self.handler = MongoHandler(self.collection)
+        self.log.addHandler(self.handler)
+    
+
     def tearDown(self):
         """ Drop used database """
         self.conn.drop_database(self.db_name)
+        self.log.removeHandler(self.handler)
         
 
     def testLogging(self):
         """ Simple logging example """
-        log = logging.getLogger('')
-        log.setLevel(logging.DEBUG)
-
-        log.addHandler(MongoHandler(self.collection))
-        log.debug('test')
+        self.log.debug('test')
 
         r = self.collection.find_one({'levelname':'DEBUG', 'msg':'test'})
         self.assertEquals(r['msg'], 'test')
 
     def testLoggingException(self):
         """ Logging example with exception """
-        log = logging.getLogger('')
-        log.setLevel(logging.DEBUG)
-
-        log.addHandler(MongoHandler(self.collection))
-
         try:
             1/0
         except ZeroDivisionError:
-            log.error('test zero division', exc_info=True)
+            self.log.error('test zero division', exc_info=True)
 
         r = self.collection.find_one({'levelname':'ERROR', 'msg':'test zero division'})
         self.assertTrue(r['exc_info'].startswith('Traceback'))
 
     def testQueryableMessages(self):
         """ Logging example with dictionary """
-        log = logging.getLogger('query')
-        log.setLevel(logging.DEBUG)
-
-        log.addHandler(MongoHandler(self.collection))
-
-        log.info({'address': '340 N 12th St', 'state': 'PA', 'country': 'US'})
-        log.info({'address': '340 S 12th St', 'state': 'PA', 'country': 'US'})
-        log.info({'address': '1234 Market St', 'state': 'PA', 'country': 'US'})
+        self.log.info({'address': '340 N 12th St', 'state': 'PA', 'country': 'US'})
+        self.log.info({'address': '340 S 12th St', 'state': 'PA', 'country': 'US'})
+        self.log.info({'address': '1234 Market St', 'state': 'PA', 'country': 'US'})
     
-        cursor = self.collection.find({'level':'info', 'msg.address': '340 N 12th St'})
+        cursor = self.collection.find({'levelname':'INFO', 'msg.address': '340 N 12th St'})
         self.assertEquals(cursor.count(), 1, "Expected query to return 1 "
             "message; it returned %d" % cursor.count())
         self.assertEquals(cursor[0]['msg']['address'], '340 N 12th St')
 
-        cursor = self.collection.find({'level':'info', 'msg.state': 'PA'})
+        cursor = self.collection.find({'levelname':'INFO', 'msg.state': 'PA'})
 
         self.assertEquals(cursor.count(), 3, "Didn't find all three documents")
